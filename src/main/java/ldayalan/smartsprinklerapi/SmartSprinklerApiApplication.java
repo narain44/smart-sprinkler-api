e63811c820253a617bb84ec46fb64e2473e79b77
Lakshmi Narayanan Dayalan <narain44@yahoo.com>
2022-07-24 11:44:02 -0400

    Initial commit

---
+++ src/main/java/ldayalan/smartsprinklerapi/SmartSprinklerApiApplication.java
@@ -0,0 +1,13 @@
+package ldayalan.smartsprinklerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSprinklerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSprinklerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ldayalan/smartsprinklerapi/SmartSprinklerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ldayalan.smartsprinklerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSprinklerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
